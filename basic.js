084e4df5441e4cd55a466919f1ba58f2e7579117
Robert Mao <zhihong.mao@gmail.com>
2013-01-05 17:04:38 -0800

    google url shorterner

--- basic.js
+++ basic.js
@@ -5,13 +5,26 @@
 // Pre-generated OAuth Token here
 
 GOOGLE_APP_ID = '407408718192.apps.googleusercontent.com';
-// !!!NOTE!!!: Change OAuth token since it normally expire in 1 hour
-GOOGLE_OATH_TOKEN = 'ya29.AHES6ZTT7GMxwT3_I-2hKDu6B2Ngwryzrti2Frrd2f9TTXtJhq4nzw';
 GOOGLE_CALENDER_URL = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
-	
+GOOGLE_URL_SHORTENER = 'https://www.googleapis.com/urlshortener/v1/url';
+
+//!!!NOTE!!!: Change OAuth token since it normally expire in 1 hour
+GOOGLE_OATH_TOKEN = 'ya29.AHES6ZRDJiOVh0ChJ-RlnekSStvrquFjNIpXV96KNMveJXLBI5ECyA';
+
+
+function shortenUrl(url)
+{
+	return $.ajax(GOOGLE_URL_SHORTENER, 		
+		{
+			type: 'POST',
+			contentType: 'application/json',
+			data:'{"longUrl": "' + url + '"}',
+		});
+}
+
 function getGoogleCalenderItem()
 {
-	$.ajax(GOOGLE_CALENDER_URL, 
+	return $.ajax(GOOGLE_CALENDER_URL, 
 			{
 				data: {
 					key: GOOGLE_APP_ID, 
@@ -20,10 +33,23 @@ function getGoogleCalenderItem()
 				headers: {
 					'Authorization': 'Bearer ' + GOOGLE_OATH_TOKEN
 				},
-				dataType:'text'
-			}).done(function(result) {
-				alert(result);
 			});
+}
+
+function handleEvents(data)
+{
+	for (var i in data.items) {
+		var event = data.items[i];
+		
+		$('#output').append(event.description||event.summary + ' ' + event.location+ ' '+ event.start.dateTime+ ' <br/>');
+	}
+}
+
+function showGoogleCalenderItem() 
+{
+	getGoogleCalenderItem().done(function(data) {
+		handleEvents(data);
+	});
 }
 
 function init()
