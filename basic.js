3d5e355bff17315627de627a082fd20b9d77b5da
Haitao Li <lht1999@hotmail.com>
2013-01-05 23:50:40 -0800

    Format event time

--- basic.js
+++ basic.js
@@ -299,3 +299,16 @@ function timeSince(date) {
     }
     return "1 minute"; //Math.floor(seconds) + " seconds";
 }
+
+function formatCalendarTime(date)
+{
+	// http://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? 'PM' : 'AM';
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	var strTime = hours + ':' + minutes + ' ' + ampm;
+	return strTime;
+}
